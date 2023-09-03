865ead4af858e805a0ac557827651dcdc6177369
Reema Khaseeb <reemafkhaseeb@gmail.com>
2023-09-03 15:50:44 +0300

    Adjust calling Bots Configuration File

--- Real-time-WeatherMonitoring-and-ReportingService/ConfigurationManager.cs
+++ Real-time-WeatherMonitoring-and-ReportingService/ConfigurationManager.cs
@@ -5,11 +5,12 @@ namespace Real_time_WeatherMonitoring_and_ReportingService
     public class ConfigurationManager
     {
         private static BotConfigurations _configurations;
+        private static readonly string ConfigFilePath = @"C:\\Users\\DELL\\Documents\\GitHub\\Real-time-WeatherMonitoring-and-ReportingService\\Real-time-WeatherMonitoring-and-ReportingService\\botConfig.json";
 
-        public static BotConfigurations GetConfiguration(string configFilePath)
+        public static BotConfigurations GetConfiguration()
         {
             // Read the configuration file and deserialize it if not already loaded
-            _configurations ??= ReadBotConfigurations(configFilePath);
+            _configurations ??= ReadBotConfigurations(ConfigFilePath);
 
             return _configurations;
         }
