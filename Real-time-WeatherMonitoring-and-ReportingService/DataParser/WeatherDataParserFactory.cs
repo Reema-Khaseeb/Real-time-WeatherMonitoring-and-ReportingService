13c3a727525e833df9fde22f85476922a10700bc
Reema Khaseeb <reemafkhaseeb@gmail.com>
2023-09-05 22:11:48 +0300

    Refactor WeatherDataParserFactory.cs class

--- Real-time-WeatherMonitoring-and-ReportingService/DataParser/WeatherDataParserFactory.cs
+++ Real-time-WeatherMonitoring-and-ReportingService/DataParser/WeatherDataParserFactory.cs
@@ -2,22 +2,33 @@
 {
     public class WeatherDataParserFactory
     {
+        private readonly Dictionary<char, Func<IWeatherDataParser>> parserFactories;
+
+        public WeatherDataParserFactory()
+        {
+            parserFactories = new Dictionary<char, Func<IWeatherDataParser>>
+            {
+                { '{', () => new JsonWeatherDataParser() },
+                { '<', () => new XmlWeatherDataParser() }
+            };
+        }
+
         public IWeatherDataParser CreateParser(string input)
         {
-            // Determine the format based on the input
-            if (input.TrimStart().StartsWith("{"))
+            var trimmedInput = input.TrimStart();
+            if (string.IsNullOrEmpty(trimmedInput))
             {
-                return new JsonWeatherDataParser();
-            }
-            else if (input.TrimStart().StartsWith("<"))
-            {
-                return new XmlWeatherDataParser();
-            }
-            else
-            {
-                Console.WriteLine("Unable to determine data format from input.");
+                Console.WriteLine("Input is empty or contains only whitespace.");
                 return null;
             }
+
+            if (parserFactories.TryGetValue(trimmedInput[0], out var parserFactory))
+            {
+                return parserFactory();
+            }
+
+            Console.WriteLine("Unable to determine data format from input.");
+            return null;
         }
     }
 }
